18b4cbf72721c5c4da7abdbe6489c89b76a64a90
Sherlock Software Inc. <sherlockwu@sherlocksoftwareinc.com>
2025-05-25 17:45:28 -0700

    Remove the DBTableName (using ObjectName)

---
+++ SQL Document Builder/Template/TemplateItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQL_Document_Builder.Template
+{
+    /// <summary>
+    /// The template item.
+    /// </summary>
+    internal class TemplateItem
+    {
+
+
+
+    }
+}
